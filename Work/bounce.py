bbdd4fa7a01e91a7679d46da4473326a1d24442a
Pavithra Eswaramoorthy <pavithraes@outlook.com>
2020-08-15 03:11:00 +0530

    hello world exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+def bounce():
+    height = 100
+    for n in range(1,11):
+        height *= 3/5
+        print (n, round(height, 4))
+    return 0
+
+if __name__ == "__main__":
+    bounce()
